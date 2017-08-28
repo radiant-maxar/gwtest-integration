b61174a240e5fcc617856008f7c0b0458e3a8389
James Yarrington <jyarrington@vt.edu>
2017-08-28 15:37:59 -0400

    sudo next yum

--- ci/Junit/installGW.sh
+++ ci/Junit/installGW.sh
@@ -13,7 +13,7 @@ install_app           => true,
 install_app_server    => true,
 http_port             => '8993',
 EOF
-yum -y --enablerepo=geowave install geowave-0.9.5-puppet.noarch >> /var/geowave_install.log
+sudo yum -y --enablerepo=geowave install geowave-0.9.5-puppet.noarch >> /var/geowave_install.log
 sh -c "puppet apply /tmp/geowave.pp"
 
 sudo -u hdfs hdfs dfs -mkdir /apps/hbase/data/lib

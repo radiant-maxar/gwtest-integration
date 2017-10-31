258816f4e1787d54e2f5c4ae3406416f742fc1b2
James Yarrington <jyarrington@vt.edu>
2017-10-31 11:41:04 -0400

    use different maven mirror

--- ci/startup.sh
+++ ci/startup.sh
@@ -2,9 +2,9 @@
 
 # Install maven.
 
-wget http://mirror.stjschools.org/public/apache/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
-tar xvf apache-maven-3.5.0-bin.tar.gz
-export M2_HOME=/home/hadoop/apache-maven-3.5.0
+wget http://apache.spinellicreations.com/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+tar xvf apache-maven-3.5.2-bin.tar.gz
+export M2_HOME=/home/hadoop/apache-maven-3.5.2
 export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
 mvn -version

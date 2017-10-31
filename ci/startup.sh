c7f003c71c7764f8abee80ddccb73e651bff8804
James Yarrington <jyarrington@vt.edu>
2017-10-31 12:01:29 -0400

    look for mvn install

--- ci/startup.sh
+++ ci/startup.sh
@@ -4,9 +4,10 @@
 
 wget http://apache.spinellicreations.com/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
 tar xvf apache-maven-3.5.2-bin.tar.gz
-export M2_HOME=/home/hadoop/apache-maven-3.5.2
+export M2_HOME=$PWD/apache-maven-3.5.2
 export M2=$M2_HOME/bin
 export PATH=$M2:$PATH
+which mvn
 mvn -version
 
 # Install git

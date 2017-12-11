9da438ac44e3aeb18a6c296ae9557c120dbad6a0
James Yarrington <jyarrington@vt.edu>
2017-12-11 13:52:25 -0500

    add sudos back

--- ci/startup.sh
+++ ci/startup.sh
@@ -11,25 +11,25 @@ which mvn
 mvn -version
 
 # Install git
-yum -y install git
+sudo yum -y install git
 
 # Get GW scripts
 cd /mnt
-wget s3.amazonaws.com/geowave/latest/scripts/sandbox/quickstart/geowave-env.sh
-wget s3.amazonaws.com/geowave/latest/scripts/emr/quickstart/KDEColorMap.sld
-wget s3.amazonaws.com/geowave/latest/scripts/emr/quickstart/SubsamplePoints.sld
+sudo wget s3.amazonaws.com/geowave/latest/scripts/sandbox/quickstart/geowave-env.sh
+sudo wget s3.amazonaws.com/geowave/latest/scripts/emr/quickstart/KDEColorMap.sld
+sudo wget s3.amazonaws.com/geowave/latest/scripts/emr/quickstart/SubsamplePoints.sld
 source /mnt/geowave-env.sh
 
 # Get gdelt data
-mkdir $STAGING_DIR/gdelt;cd $STAGING_DIR/gdelt
-wget http://data.gdeltproject.org/events/md5sums
+sudo mkdir $STAGING_DIR/gdelt;cd $STAGING_DIR/gdelt
+sudo wget http://data.gdeltproject.org/events/md5sums
 for file in `cat md5sums | cut -d' ' -f3 | grep "^${TIME_REGEX}"` ; \
-do wget http://data.gdeltproject.org/events/$file ; done
+do sudo wget http://data.gdeltproject.org/events/$file ; done
 md5sum -c md5sums 2>&1 | grep "^${TIME_REGEX}"
 
 # Install Gdal
 cd /mnt
-wget http://demo.geo-solutions.it/share/github/imageio-ext/releases/1.1.X/1.1.7/native/gdal/linux/gdal192-CentOS5.8-gcc4.1.2-x86_64.tar.gz
+sudo wget http://demo.geo-solutions.it/share/github/imageio-ext/releases/1.1.X/1.1.7/native/gdal/linux/gdal192-CentOS5.8-gcc4.1.2-x86_64.tar.gz
 tar -xvf gdal192-CentOS5.8-gcc4.1.2-x86_64.tar.gz
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/mnt
 cd ~

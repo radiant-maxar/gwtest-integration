f8b1bb1a6d64fb3acd4475616ebbedbd755c5a30
James Yarrington <jyarrington@vt.edu>
2018-04-06 15:08:36 -0400

    fix relative path

--- ci/runtests_jupyter.sh
+++ ci/runtests_jupyter.sh
@@ -3,7 +3,7 @@
 # Clone test repo
 git clone https://github.com/venicegeo/gwtest-integration.git
 
-cd Jupyter
+cd gwtest-integration/ci/Jupyter
 
 jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.allow_errors=True --output results.ipynb Index.ipynb
 python jupyter_tester.py results.ipynb expected_outputs.json

a722275a4b2f919878dcaeb74e30050a178bd057
James Yarrington <jyarrington@vt.edu>
2018-04-06 15:31:30 -0400

    find for debug

--- ci/runtests_jupyter.sh
+++ ci/runtests_jupyter.sh
@@ -3,7 +3,11 @@
 # Clone test repo
 git clone https://github.com/venicegeo/gwtest-integration.git
 
-cd gwtest-integration/ci/Jupyter
+find
+
+cd gwtest-integration
+cd ci
+cd Jupyter
 
 jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --ExecutePreprocessor.allow_errors=True --output results.ipynb Index.ipynb
 python jupyter_tester.py results.ipynb expected_outputs.json

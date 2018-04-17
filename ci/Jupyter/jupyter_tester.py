a6b13c6c6f30b89545331f4599456df6bda0e6d4
James Yarrington <jyarrington@vt.edu>
2018-04-17 11:49:30 -0400

    better variable names.  Reorder key

--- ci/Jupyter/jupyter_tester.py
+++ ci/Jupyter/jupyter_tester.py
@@ -3,14 +3,14 @@
 import json, re, sys, subprocess, os
 
 # Convert input files to JSON.
-expected_file = json.load(open(sys.argv[1]))
-orig_notebook = expected_file["path"]
-regexes = expected_file["outputs"]
-notebook_path = os.path.dirname(orig_notebook)
+expected_responses_json = json.load(open(sys.argv[1]))
+notebook_under_test = expected_responses_json["path"]
+expected_command_responses = expected_responses_json["outputs"]
+notebook_path = os.path.dirname(notebook_under_test)
 # Execute the notebook
 subprocess.call(["jupyter", "nbconvert", "--to", "notebook", "--execute", "--ExecutePreprocessor.timeout=600", "--ExecutePreprocessor.interrupt_on_timeout=True",
 	"--ExecutePreprocessor.allow_errors=True", "--ExecutePreprocessor.kernel_name=pythonwithpixiedustspark21",
-	"--output", "results.ipynb", orig_notebook
+	"--output", "results.ipynb", notebook_under_test
 	])
 results = json.load(open(notebook_path + "/results.ipynb"))
 
@@ -20,21 +20,21 @@ cell_outputs = [cell["outputs"] for cell in results["cells"] if "outputs" in cel
 # Will become 1 if failure occurs
 exit_code = 0
 
-if not len(cell_outputs) == len(regexes):
+if not len(cell_outputs) == len(expected_command_responses):
 	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	print("WARNING: There is not an equal number of")
 	print("         Results and Matching Patterns!")
 	print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 	exit_code = 1
 
-for i, regex in enumerate(regexes):
+for i, expected_command_response in enumerate(expected_command_responses):
 	# For each output/regex pair:
 
 	print("")
 	print("Checking Output %d" % i)
 	print("-----------------")
 
-	if not regex:
+	if not expected_command_response:
 		print(" - Skipped")
 		continue
 
@@ -46,13 +46,13 @@ for i, regex in enumerate(regexes):
 		exit_code = 1 # Should already be 1.
 		continue
 
-	for i, regex_section in enumerate(regex):
+	for i, expected_output in enumerate(expected_command_response):
 		# Check each regex in the list of regexes for the section
 		# Each regex section should pair with an output.
 
 		# Get the output and the regex to compare it to for this iteration.
 		output = cell_output[i]
-		p = re.compile(regex_section)
+		p = re.compile(expected_output)
 
 		if "ename" in output:
 			# For errors, mark as failure, print.
@@ -71,7 +71,7 @@ for i, regex in enumerate(regexes):
 				for line in output["text"]:
 					print("     - ", line)
 				print("   - Expected Pattern:")
-				print("     - ", regex_section)
+				print("     - ", expected_output)
 		else:
 			# Mark as a failure if pass/failure can't be determined.
 			exit_code = 1
